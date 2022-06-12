95fd58a54e89042117e2785e0701029c144f4e91
Sam Thompson <samthompson292@gmail.com>
2022-06-12 13:13:54 +0100

    added server endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
-const PORT = 5001;
+const { errorHandler } = require('./middleware/errorMiddleware');
+const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use('/api/goals', require('./routes/goalRoates'));
+
+app.use(errorHandler);
+
 app.listen(PORT, () => console.log(`Server starting on port ${PORT}`));
