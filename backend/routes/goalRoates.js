47df1db53d0dcdaac0f312be6ad674e1ebd7a222
Sam Thompson <samthompson292@gmail.com>
2022-06-12 13:45:49 +0100

    connected mongodb and added crud functionality

--- backend/routes/goalRoates.js
+++ backend/routes/goalRoates.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController');
 
 router.route('/').get(getGoals).post(setGoal)
-router.route(':id').put(updateGoal).delete(deleteGoal)
+router.route('/:id').put(updateGoal).delete(deleteGoal)
 
 module.exports = router;
