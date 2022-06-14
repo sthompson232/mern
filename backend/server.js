097141aa6c002e5a44e91a995bc37bc79e7ce657
Sam Thompson <samthompson292@gmail.com>
2022-06-14 08:45:55 +0100

    delete functionality

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,33 @@
-const express = require('express');
-const colors = require('colors');
-const cors = require('cors');
-const dotenv = require('dotenv').config();
-const { errorHandler } = require('./middleware/errorMiddleware');
-const connectDB = require('./config/db');
-const PORT = process.env.PORT || 5000;
-const whitelist = ["http://localhost:3000"];
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const port = process.env.PORT || 5000
 
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"))
-    }
-  },
-  credentials: true,
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+app.use('/api/users', require('./routes/userRoutes'))
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  )
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'))
 }
 
-connectDB();
-const app = express();
+app.use(errorHandler)
 
-app.use(cors(corsOptions));
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-
-app.use('/api/goals', require('./routes/goalRoutes'));
-app.use('/api/users', require('./routes/userRoutes'));
-
-app.use(errorHandler);
-
-app.listen(PORT, () => console.log(`Server starting on port ${PORT}`));
+app.listen(port, () => console.log(`Server started on port ${port}`))
--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,39 +1,38 @@
-import axios from 'axios';
+import axios from 'axios'
 
-const ROOT_URL = 'http://localhost:5000'
-const API_URL = `${ROOT_URL}/api/users/`;
+const API_URL = '/api/users/'
 
 // Register user
 const register = async (userData) => {
-  const response = await axios.post(API_URL, userData);
+  const response = await axios.post(API_URL, userData)
 
   if (response.data) {
-    localStorage.setItem('user', JSON.stringify(response.data));
+    localStorage.setItem('user', JSON.stringify(response.data))
   }
 
-  return response.data;
+  return response.data
 }
 
 // Login user
 const login = async (userData) => {
-  const response = await axios.post(`${API_URL}login`, userData);
+  const response = await axios.post(API_URL + 'login', userData)
 
   if (response.data) {
-    localStorage.setItem('user', JSON.stringify(response.data));
+    localStorage.setItem('user', JSON.stringify(response.data))
   }
 
-  return response.data;
+  return response.data
 }
 
-// Logout
+// Logout user
 const logout = () => {
-  localStorage.removeItem('user');
-};
+  localStorage.removeItem('user')
+}
 
 const authService = {
   register,
   logout,
-  login
-};
+  login,
+}
 
-export default authService;
+export default authService
